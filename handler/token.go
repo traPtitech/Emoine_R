d48d1f2624a23711bd249150ca6b6eaccd68d63c
HinaruhiGithub <letshinaruhi@gmail.com>
2023-07-07 22:12:17 +0900

    impl token/:token first

--- handler/token.go
+++ handler/token.go
@@ -1,9 +1,13 @@
 package handler
 
 import (
+	"database/sql"
+	"errors"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+
+	"github.com/traPtitech/Emoine_R/handler/schema"
 )
 
 func GetToken(c echo.Context) error {
@@ -14,8 +18,14 @@ func PostToken(c echo.Context) error {
 	return c.String(http.StatusNotImplemented, "未実装です")
 }
 
+// stringなキーのtokenから構造体のtokenを得る。
 func GetTokenFromToken(c echo.Context) error {
-	return c.String(http.StatusNotImplemented, "未実装です")
+	token_string := c.Param("token")
+	var token_struct schema.Token
+	if err := model.Db.Get(&token_struct, "SELECT * FROM token WHERE token='"+token_string+"'"); errors.Is(err, sql.ErrNoRows) {
+		return c.String(http.StatusNotFound, "tokenが見つかりませんでした: "+err.Error())
+	}
+	return c.JSON(http.StatusOK, token_struct)
 }
 
 func PatchTokenFromToken(c echo.Context) error {
