57218101c86ece8f08ab529dc6b0106a3d0d7c31
HinaruhiGithub <letshinaruhi@gmail.com>
2023-07-08 01:24:36 +0900

    modify token.go for token.xo.go

--- handler/token.go
+++ handler/token.go
@@ -1,13 +1,12 @@
 package handler
 
 import (
-	"database/sql"
-	"errors"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 
-	"github.com/traPtitech/Emoine_R/handler/schema"
+	"github.com/traPtitech/Emoine_R/model"
+	"github.com/traPtitech/Emoine_R/model/dbschema"
 )
 
 func GetToken(c echo.Context) error {
@@ -21,8 +20,9 @@ func PostToken(c echo.Context) error {
 // stringなキーのtokenから構造体のtokenを得る。
 func GetTokenFromToken(c echo.Context) error {
 	token_string := c.Param("token")
-	var token_struct schema.Token
-	if err := model.Db.Get(&token_struct, "SELECT * FROM token WHERE token='"+token_string+"'"); errors.Is(err, sql.ErrNoRows) {
+	token_struct, err := dbschema.TokenByToken(c.Request().Context(), model.DB, token_string)
+
+	if err != nil {
 		return c.String(http.StatusNotFound, "tokenが見つかりませんでした: "+err.Error())
 	}
 	return c.JSON(http.StatusOK, token_struct)
