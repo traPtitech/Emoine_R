8aa374a3e13bccb2975ae08c973e19bca497f860
ras0q <ras0q@users.noreply.github.com>
2023-07-29 13:07:17 +0900

    Revert ":sparkles: add format on go generate"
    This reverts commit e1dbf796f433946d25fa9ccc4fae3c0a43ffbcd4.

--- api/proto/generate.go
+++ api/proto/generate.go
@@ -1,4 +1,3 @@
 //go:generate go run github.com/bufbuild/buf/cmd/buf@latest lint
-//go:generate go run github.com/bufbuild/buf/cmd/buf@latest format --diff --exit-code
 //go:generate go run github.com/bufbuild/buf/cmd/buf@latest generate
 package main
