2f6b7fd391b927aaf481f0472b95bb3f73df9a6d
pirosiki197 <lingmukangtai825@gmail.com>
2023-08-11 13:04:29 +0900

    GetTokensを書き換えた

--- handler/token.go
+++ handler/token.go
@@ -1,12 +1,20 @@
 package handler
 
 import (
+	"context"
+	"errors"
 	"net/http"
 
+	"github.com/bufbuild/connect-go"
 	"github.com/labstack/echo/v4"
 	"github.com/traPtitech/Emoine_R/model"
 	"github.com/traPtitech/Emoine_R/model/dbschema"
+	emoine_rv1 "github.com/traPtitech/Emoine_R/pkg/pbgen/emoine_r/v1"
 )
+
+func (h *AdminAPIHandler) GetTokens(ctx context.Context, req *connect.Request[emoine_rv1.GetTokensRequest]) (*connect.Response[emoine_rv1.GetTokensResponse], error) {
+	return nil, connect.NewError(connect.CodeUnimplemented, errors.New("未実装です"))
+}
 
 func GetTokens(c echo.Context) error {
 	return c.String(http.StatusNotImplemented, "未実装です")
