b94ce00d3c612c456059027edfc66f49941b1528
pirosiki197 <lingmukangtai825@gmail.com>
2023-08-22 14:32:21 +0900

    interfaceを返すようにした

--- handler/admin_handler.go
+++ handler/admin_handler.go
@@ -2,13 +2,15 @@ package handler
 
 import (
 	"log/slog"
+
+	"github.com/traPtitech/Emoine_R/pkg/pbgen/emoine_r/v1/emoine_rv1connect"
 )
 
 type AdminAPIHandler struct {
 	logger *slog.Logger
 }
 
-func NewAdminAPIHandler(logger *slog.Logger) *AdminAPIHandler {
+func NewAdminAPIHandler(logger *slog.Logger) emoine_rv1connect.AdminAPIServiceHandler {
 	return &AdminAPIHandler{
 		logger: logger,
 	}
--- handler/general_handler.go
+++ handler/general_handler.go
@@ -2,13 +2,15 @@ package handler
 
 import (
 	"log/slog"
+
+	"github.com/traPtitech/Emoine_R/pkg/pbgen/emoine_r/v1/emoine_rv1connect"
 )
 
 type GeneralAPIHandler struct {
 	logger *slog.Logger
 }
 
-func NewGeneralAPIHandler(logger *slog.Logger) *GeneralAPIHandler {
+func NewGeneralAPIHandler(logger *slog.Logger) emoine_rv1connect.GeneralAPIServiceHandler {
 	return &GeneralAPIHandler{
 		logger: logger,
 	}
