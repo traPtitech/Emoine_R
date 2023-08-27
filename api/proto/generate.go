ea11cdf48fbeaa011ffa16fe6792da7539d66149
ras0q <ras0q@users.noreply.github.com>
2023-08-27 23:27:01 +0900

    :bug: fix buf breaking's against dir

--- api/proto/generate.go
+++ api/proto/generate.go
@@ -1,4 +1,5 @@
 //go:generate go run github.com/bufbuild/buf/cmd/buf@latest lint
 //go:generate go run github.com/bufbuild/buf/cmd/buf@latest format --diff --exit-code
+//go:generate go run github.com/bufbuild/buf/cmd/buf@latest breaking . --against https://github.com/traPtitech/Emoine_R.git#subdir=api/proto
 //go:generate go run github.com/bufbuild/buf/cmd/buf@latest generate
 package main
