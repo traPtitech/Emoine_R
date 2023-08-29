59a791ac2239ea9fd934cab5b7b1d1c01e9fac62
pirosiki197 <lingmukangtai825@gmail.com>
2023-08-29 11:16:34 +0900

    NullString用に書き換えた

--- pkg/pbconv/comment.go
+++ pkg/pbconv/comment.go
@@ -1,6 +1,7 @@
 package pbconv
 
 import (
+	"github.com/samber/lo"
 	"github.com/traPtitech/Emoine_R/model/dbschema"
 	emoine_rv1 "github.com/traPtitech/Emoine_R/pkg/pbgen/emoine_r/v1"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -13,7 +14,7 @@ func FromDBComment(c dbschema.Comment) *emoine_rv1.Comment {
 		Username:    c.UserID,
 		Text:        c.Text,
 		IsAnonymous: c.IsAnonymous,
-		Color:       c.Color.String,
+		Color:       lo.Ternary(c.Color.Valid, c.Color.String, ""),
 		CreatedAt:   timestamppb.New(c.CreatedAt),
 	}
 }
