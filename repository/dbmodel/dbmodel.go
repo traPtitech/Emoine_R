0fea0b96374c9736b83b16f9435fd403751eafca
ras0q <ras0q@users.noreply.github.com>
2023-09-23 01:43:25 +0900

    :boom: comments,reactions: xo->bun

--- repository/dbmodel/dbmodel.go
+++ repository/dbmodel/dbmodel.go
@@ -18,4 +18,31 @@ type Event struct {
 	Description sql.NullString `bun:""`
 	StartedAt   time.Time      `bun:",notnull,default:CURRENT_TIMESTAMP"`
 	EndedAt     sql.NullTime   `bun:""`
+
+	// event has many comments
+	Comments []*Comment `bun:"rel:has-many,join:id=event_id"`
+	// event has many reactions
+	Reactions []*Reaction `bun:"rel:has-many,join:id=event_id"`
+}
+
+type Comment struct {
+	bun.BaseModel `bun:"table:comments"`
+
+	ID          uuid.UUID      `bun:",pk,notnull,type:uuid,default:UUID()"`
+	UserID      string         `bun:",notnull"` // TODO: usernameにする？
+	EventID     uuid.UUID      `bun:",notnull,type:uuid"`
+	Text        string         `bun:",notnull"`
+	IsAnonymous bool           `bun:",notnull"`
+	Color       sql.NullString `bun:""`
+	CreatedAt   time.Time      `bun:",notnull,default:CURRENT_TIMESTAMP"`
+}
+
+type Reaction struct {
+	bun.BaseModel `bun:"table:reactions"`
+
+	ID        uuid.UUID `bun:",pk,notnull,type:uuid,default:UUID()"`
+	UserID    string    `bun:",notnull"`
+	EventID   uuid.UUID `bun:",notnull,type:uuid"`
+	StampID   uuid.UUID `bun:",notnull,type:uuid"`
+	CreatedAt time.Time `bun:",notnull,default:CURRENT_TIMESTAMP"`
 }
