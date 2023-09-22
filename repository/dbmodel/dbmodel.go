c3d6d04fc9436b237a2b1888a88a4630df48a627
ras0q <ras0q@users.noreply.github.com>
2023-09-23 01:52:30 +0900

    :boom: tokens: xo->bun

--- repository/dbmodel/dbmodel.go
+++ repository/dbmodel/dbmodel.go
@@ -23,6 +23,8 @@ type Event struct {
 	Comments []*Comment `bun:"rel:has-many,join:id=event_id"`
 	// event has many reactions
 	Reactions []*Reaction `bun:"rel:has-many,join:id=event_id"`
+	// event has many tokens
+	Tokens []*Token `bun:"rel:has-many,join:id=event_id"`
 }
 
 type Comment struct {
@@ -45,4 +47,17 @@ type Reaction struct {
 	EventID   uuid.UUID `bun:",notnull,type:uuid"`
 	StampID   uuid.UUID `bun:",notnull,type:uuid"`
 	CreatedAt time.Time `bun:",notnull,default:CURRENT_TIMESTAMP"`
+}
+
+type Token struct {
+	bun.BaseModel `bun:"table:token"`
+
+	ID          uuid.UUID      `bun:",pk,notnull,type:uuid,default:UUID()"`
+	Value       string         `bun:",notnull"`
+	UserID      string         `bun:",notnull"`
+	CreatorID   string         `bun:",notnull"`
+	EventID     uuid.UUID      `bun:",notnull,type:uuid"`
+	Description sql.NullString `bun:""`
+	CreatedAt   time.Time      `bun:",notnull,default:CURRENT_TIMESTAMP"`
+	ExpireAt    sql.NullTime   `bun:""`
 }
