f3eefa7f567b6fb7e8e93a2c3828cfb890b80a07
ras0q <ras0q@users.noreply.github.com>
2023-09-23 15:55:59 +0900

    :fire: remove debug log

--- repository/migrations/0_create_tables.go
+++ repository/migrations/0_create_tables.go
@@ -2,7 +2,6 @@ package migrations
 
 import (
 	"context"
-	"log/slog"
 
 	"github.com/traPtitech/Emoine_R/repository/dbmodel"
 	"github.com/uptrace/bun"
@@ -10,7 +9,6 @@ import (
 
 func init() {
 	up := func(ctx context.Context, db *bun.DB) error {
-		slog.Info("migrate up")
 		tx, err := db.BeginTx(ctx, nil)
 		if err != nil {
 			return err
@@ -31,7 +29,6 @@ func init() {
 	}
 
 	down := func(ctx context.Context, db *bun.DB) error {
-		slog.Info("migrate down")
 		if _, err := db.NewDropTable().Model(&dbmodel.Event{}).IfExists().Exec(ctx); err != nil {
 			return err
 		}
