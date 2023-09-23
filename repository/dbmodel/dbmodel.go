73a6e49dbc02fdb5ee3860f8b2c3eaa2f8829a0e
ras0q <ras0q@users.noreply.github.com>
2023-09-23 14:34:17 +0900

    :adhesive_bandage: fix bun tag

--- repository/dbmodel/dbmodel.go
+++ repository/dbmodel/dbmodel.go
@@ -12,10 +12,10 @@ type Event struct {
 	bun.BaseModel `bun:"table:events"`
 
 	ID          uuid.UUID      `bun:",pk,notnull,type:uuid,default:UUID()"`
-	VideoID     string         `bun:",notnull"`
-	Title       string         `bun:",notnull"`
-	Thumbnail   string         `bun:",notnull"`
-	Description sql.NullString `bun:""`
+	VideoID     string         `bun:",notnull,type:char(11)"`
+	Title       string         `bun:",notnull,nullzero,type:varchar(64)"`
+	Thumbnail   string         `bun:",notnull,nullzero"`
+	Description sql.NullString `bun:",type:text"`
 	StartedAt   time.Time      `bun:",notnull,default:CURRENT_TIMESTAMP"`
 	EndedAt     sql.NullTime   `bun:""`
 
@@ -31,11 +31,11 @@ type Comment struct {
 	bun.BaseModel `bun:"table:comments"`
 
 	ID          uuid.UUID      `bun:",pk,notnull,type:uuid,default:UUID()"`
-	UserID      string         `bun:",notnull"` // TODO: usernameにする？
+	UserID      string         `bun:",notnull,nullzero,type:varchar(32)"` // TODO: usernameにする？
 	EventID     uuid.UUID      `bun:",notnull,type:uuid"`
-	Text        string         `bun:",notnull"`
+	Text        string         `bun:",notnull,nullzero,type:text"`
 	IsAnonymous bool           `bun:",notnull"`
-	Color       sql.NullString `bun:""`
+	Color       sql.NullString `bun:",notnull,nullzero,type:varchar(7)"`
 	CreatedAt   time.Time      `bun:",notnull,default:CURRENT_TIMESTAMP"`
 }
 
@@ -43,7 +43,7 @@ type Reaction struct {
 	bun.BaseModel `bun:"table:reactions"`
 
 	ID        uuid.UUID `bun:",pk,notnull,type:uuid,default:UUID()"`
-	UserID    string    `bun:",notnull"`
+	UserID    string    `bun:",notnull,nullzero,type:varchar(32)"`
 	EventID   uuid.UUID `bun:",notnull,type:uuid"`
 	StampID   uuid.UUID `bun:",notnull,type:uuid"`
 	CreatedAt time.Time `bun:",notnull,default:CURRENT_TIMESTAMP"`
@@ -53,11 +53,11 @@ type Token struct {
 	bun.BaseModel `bun:"table:token"`
 
 	ID          uuid.UUID      `bun:",pk,notnull,type:uuid,default:UUID()"`
-	Value       string         `bun:",notnull"`
-	UserID      string         `bun:",notnull"`
-	CreatorID   string         `bun:",notnull"`
+	Value       string         `bun:",notnull,type:char(44)"`
+	UserID      string         `bun:",notnull,nullzero,type:varchar(32)"`
+	CreatorID   string         `bun:",notnull,nullzero,type:varchar(32)"`
 	EventID     uuid.UUID      `bun:",notnull,type:uuid"`
-	Description sql.NullString `bun:""`
+	Description sql.NullString `bun:",type:text"`
 	CreatedAt   time.Time      `bun:",notnull,default:CURRENT_TIMESTAMP"`
 	ExpireAt    sql.NullTime   `bun:""`
 }
